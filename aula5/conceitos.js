0a5e78cbc502747e9273228285b8350b5581dcb4
paula_tonial <paula.tonial@sap.com>
2025-07-31 15:44:17 -0300

    reorganizando pastas

---
+++ aula5/conceitos.js
@@ -0,0 +1,13 @@
+let idade = 19;
+let nome = "Paula Tonial";
+let ativo = true;
+let nota = 9.3;
+let cidade = "Porto Alegre";
+
+console.log(`
+            Idade: ${idade}
+            Nome: ${nome}
+            Ativo: ${ativo}
+            Nota: ${nota}
+            Cidade: ${cidade}`);
+        
